3160d7247ee42b27260d9b136110adf1d0493dc3
cs-cognitome <gaya.scienza.v@gmail.com>
2019-04-21 18:45:32 +0300

    calculating box size

--- src/App.js
+++ src/App.js
@@ -39,7 +39,16 @@ class App extends Component {
     const image = document.getElementById('inputimage'); 
     const width = Number(image.width); 
     const height = Number(image.height); 
-    console.log(width, height); 
+    return { 
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottomRow * height)
+    }
+  }
+
+  displayFaceBox = (box) => { 
+    this.setState({box: box}); 
   }
 
   onInputChange = (event) => { 
